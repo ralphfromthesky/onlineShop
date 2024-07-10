4be1bcbd040f4b9408e2888d0e7ca081716e022d
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-07-10 10:43:01 +0800

    new

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -5,14 +5,13 @@ import AntCarousel from "../antUI/carousel";
 import { NotificationOutlined} from "@ant-design/icons";
 
 
-
 const Main = (props: any) => {
   return (
-    <div className="flex flex-col w-screen h-screen">
-      <div>
+    <div className="flex flex-col w-screen h-screen relative">
+      <div className="fixed w-full z-10">
         <Header />
       </div>
-      <div className="flex">
+      <div className="flex mt-[15vh]">
         <div>
           <Sidebar />
         </div>
@@ -20,7 +19,7 @@ const Main = (props: any) => {
           <div className="w-[80vw] border-2 border-color-1 h-[80vh] bg-[white]">
             <div className="px-[1rem] pt-[1rem]"><AntCarousel/></div>
             <div className="mx-[1rem] my-[.5rem]"><span><NotificationOutlined className="text-[2rem] text-color-5"/></span><span></span></div>
-          <div> {props.children}</div>
+          <div className="border-2 border-red h-[100vh]"> {props.children}</div>
           </div>
         </div>
       </div>
--- src/view/kidsWear.tsx
+++ src/view/kidsWear.tsx
@@ -1,20 +1,36 @@
+import { useEffect, useState } from "react";
 import Main from "../main/main";
+import axios from "axios";
 
 const KidsWear = () => {
+  const [data, setdata] = useState<any>();
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("'http://localhost:5000/api/students");
+
+        console.log(response.data)
+      
+    } catch (error) {console.log(error)}
+  };
+  useEffect(() => {
+    getData()
+  }, []);
   return (
     <Main>
       <div className="text-[black]">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-      consequatur illo repellat facere doloremque perspiciatis numquam pariatur
-      rerum qui suscipit sit labore excepturi deleniti veniam maiores alias odio
-      vitae enim nemo laudantium natus, necessitatibus recusandae dolorum! A
-      exercitationem soluta nemo quis odit, rem voluptatem pariatur quidem
-      suscipit repudiandae dolorum, iure reprehenderit cum sint non incidunt?
-      Mollitia, hic rerum nihil eos eligendi eum rem! Eum veniam amet
-      voluptatibus temporibus soluta? Ducimus molestias quos maxime voluptates
-      porro aut doloribus rem. Qui fugiat architecto temporibus ad eum minima
-      eligendi animi debitis accusamus! Maiores mollitia, nulla enim unde natus
-      fugit saepe earum obcaecati dolorum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        consequatur illo repellat facere doloremque perspiciatis numquam
+        pariatur rerum qui suscipit sit labore excepturi deleniti veniam maiores
+        alias odio vitae enim nemo laudantium natus, necessitatibus recusandae
+        dolorum! A exercitationem soluta nemo quis odit, rem voluptatem pariatur
+        quidem suscipit repudiandae dolorum, iure reprehenderit cum sint non
+        incidunt? Mollitia, hic rerum nihil eos eligendi eum rem! Eum veniam
+        amet voluptatibus temporibus soluta? Ducimus molestias quos maxime
+        voluptates porro aut doloribus rem. Qui fugiat architecto temporibus ad
+        eum minima eligendi animi debitis accusamus! Maiores mollitia, nulla
+        enim unde natus fugit saepe earum obcaecati dolorum!
+
       </div>
     </Main>
   );
